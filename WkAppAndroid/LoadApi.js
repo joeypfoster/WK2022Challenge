0c84439870b5edfda6710ac9a8219243410418cb
picocode <38665815+picocode1@users.noreply.github.com>
2022-12-14 19:33:41 +0100

    Update LoadApi.js

--- WkAppAndroid/LoadApi.js
+++ WkAppAndroid/LoadApi.js
@@ -25,44 +25,4 @@ export function loadapi() {
 			request.send();  
 		}, i * 100)
 	}
-
-	for (var value of ["teams", "matches"]) {
-	}
-	
-
-    // const teams = () => {
-    //     // console.log(n);
-    //     request.onreadystatechange = (e) => {
-	// 		console.log(1)
-	// 		console.log(1)
-    //         if (request.readyState !== 4) return;
-    //         if (request.status === 200) {
-    //             var data = JSON.parse(request.responseText);
-	// 			console.log('SETTING TEAMS')
-    //             AsyncStorage.setItem("teams", JSON.stringify(data));
-    //         } else {
-    //             console.warn('error');
-    //         }
-    //     };  
-    // }
-    // request.open('GET', 'https://worldcupjson.net/teams');
-    // request.send();  
-    // teams();
-
-    // var request2 = new XMLHttpRequest();
-    // const matches = () => {
-    //     // console.log(n);
-    //     request2.onreadystatechange = (e) => {
-    //         if (request2.readyState !== 4) return;
-    //         if (request2.status === 200) {
-    //             var data = JSON.parse(request2.responseText);
-    //             AsyncStorage.setItem("matches", JSON.stringify(data));
-    //         } else {
-    //             console.warn('error');
-    //         }
-    //     };  
-    // }
-    // request2.open('GET', 'https://worldcupjson.net/matches');
-    // request2.send();  
-    // matches();
 }

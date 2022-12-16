4b31c123184fae9e43cb9b180db178f0bc9aa722
Jarno Sijben <91888926+ClosingYard@users.noreply.github.com>
2022-12-16 10:28:57 +0100

    Update Info.js

--- WkAppAndroid/Info.js
+++ WkAppAndroid/Info.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, BackHandler, navigation } from 'react-native';
+import { StyleSheet, Text, Image, View, BackHandler, navigation } from 'react-native';
 import React, {useEffect, useState, setNativeProps} from "react"
 
 const WK = require('./Api')
@@ -29,18 +29,37 @@ const Info = ({ route, navigation}) => {
 		return () => BackHandler.removeEventListener("hardwareBackPress", backClick)
 	}, []);
 
+	let imageLink = (team) => {
+		let baseUrl = 'https://cdn.countryflags.com/thumbs/'
+
+		if (team == 'United States') {
+			return `${baseUrl}united-states-of-america/flag-400.png`
+		} else {
+			return `${baseUrl}${team.toLowerCase().replace('korea republic', 'south-korea').replace(' ', '-')}/flag-400.png`
+		}
+	}
+	
+
 	let returnInfo = () => {
 		console.log(info)
 		if (info.length) {
 			return (
 				info.map((item, index) => (
 				  <View key={index}>
+					<View style={styles.containerFlags}>
+						<Image style={styles.flag} source={{ uri: imageLink(item.home_team.name)}}/>
+						<Image style={styles.flag} source={{ uri: imageLink(item.away_team.name)}}/>
+					</View>	
+					<View style={styles.textInfo}>
 					<Text>Winner: {item.winner}</Text>
 					<Text>Location: {item.location}</Text>
 					<Text>Attendance: {item.attendance}</Text>
 					<Text>Stage: {item.stage_name}</Text>
 					<Text>Home Team: {item.home_team_country}</Text>
 					<Text>Away Team: {item.away_team_country}</Text>
+					</View>
+
+					
 				  </View>
 				))
 			)
@@ -61,6 +80,36 @@ const styles = StyleSheet.create({
 		backgroundColor: '#fff',
 		alignItems: 'center',
 		justifyContent: 'center',
+		borderColor:'black',
+		borderWidth:24,
+		
 	},
+
+	flag: {
+		alignContent:'center',
+		width: 400 / 6,
+		height: 288 / 4,
+		flex: 1,
+		flexDirection: 'row',
+		justifyContent: 'center',
+		alignItems: 'center',
+		margin: 10,
+		padding: 10,
+		borderWidth: 2,
+		borderRadius: 5,
+		borderColor: 'black',
+		backgroundColor: '#fff',
+	},
+
+	containerFlags: {
+		alignItems: 'center',
+		flex: 1,
+		flexDirection: 'row',
+		margin: 10,
+		padding: 10,
+		borderWidth: 1,
+
+	},
+
 });
 export default Info;
